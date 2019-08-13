724edc4303963786fe56dcedc1a7688f0ee40912
Sebastiaan Stok <s.stok@rollerscapes.net>
2019-08-13 15:50:38 +0200

    Fix autofill option handling
    The autofill_variables option expects either an array or string with names separated by a `,` .
    As not all variables should/might be used.

--- src/CacheWarmer/RouteRedirectMappingWarmer.php
+++ src/CacheWarmer/RouteRedirectMappingWarmer.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace Rollerworks\Bundle\RouteAutofillBundle\CacheWarmer;
 
 use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmer;
+use Symfony\Component\Routing\CompiledRoute;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouterInterface;
 use function array_fill_keys;
@@ -35,7 +36,7 @@ final class RouteRedirectMappingWarmer extends CacheWarmer
      *
      * @return false
      */
-    public function isOptional()
+    public function isOptional(): bool
     {
         return false;
     }
@@ -46,15 +47,77 @@ final class RouteRedirectMappingWarmer extends CacheWarmer
 
         /** @var Route $route */
         foreach ($this->router->getRouteCollection() as $routeName => $route) {
-            if (!$route->hasOption('autofill_variables')) {
+            $autoFillVariables = $route->getOption('autofill_variables');
+
+            if (null === $autoFillVariables || false === $autoFillVariables) {
                 continue;
             }
 
             $compiledRoute = $route->compile();
+            $variableNames = $this->getVariableNames($autoFillVariables, $compiledRoute);
+
+            if ($variableNames === []) {
+                continue;
+            }
+
             // Use a boolean value to speed-up the array look-up process
-            $mapping[$routeName] = array_fill_keys($compiledRoute->getVariables(), true);
+            $mapping[$routeName] = array_fill_keys($variableNames, true);
         }
 
-        $this->writeCacheFile($cacheDir.'/route_autofill_mapping.php', sprintf("<?php return %s;\n", var_export($mapping, true)));
+        $this->writeCacheFile($cacheDir.'/route_autofill_mapping.php', sprintf("<?php return %s;\n", self::export($mapping)));
+    }
+
+    /**
+     * @param string|array|bool $autoFillVariables
+     */
+    private function getVariableNames($autoFillVariables, CompiledRoute $compiledRoute): array
+    {
+        if (true === $autoFillVariables) {
+            return $compiledRoute->getVariables();
+        }
+
+        if (\is_string($autoFillVariables)) {
+            return array_map('trim', explode(',', $autoFillVariables));
+        }
+
+        return \is_array($autoFillVariables) ? $autoFillVariables : [];
+    }
+
+    /**
+     * @see https://github.com/symfony/symfony/blob/4.4/src/Symfony/Component/VarExporter/Internal/Exporter.php
+     *
+     * @author Nicolas Grekas <p@tchwork.com>
+     */
+    private static function export($value, string $indent = '')
+    {
+        switch (true) {
+            case \is_int($value) || \is_float($value) || \is_string($value): return var_export($value, true);
+            case [] === $value: return '[]';
+            case false === $value: return 'false';
+            case true === $value: return 'true';
+            case null === $value: return 'null';
+            case '' === $value: return "''";
+        }
+
+        $subIndent = $indent.'    ';
+
+        if (\is_array($value)) {
+            $j = -1;
+            $code = '';
+            foreach ($value as $k => $v) {
+                $code .= $subIndent;
+                if (!\is_int($k) || 1 !== $k - $j) {
+                    $code .= self::export($k, $subIndent).' => ';
+                }
+                if (\is_int($k) && $k > $j) {
+                    $j = $k;
+                }
+                $code .= self::export($v, $subIndent).",\n";
+            }
+
+            return "[\n".$code.$indent.']';
+        }
+
+        throw new \UnexpectedValueException(sprintf('Cannot export value of type "%s".', \is_object($value) ? \get_class($value) : \gettype($value)));
     }
 }
